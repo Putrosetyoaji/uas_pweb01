99d76cf7342a07d2ad89cfd8d3491ea8d4619a26
ImamAriefBudiman <imamarief29@gmail.com>
2024-01-13 21:42:04 +0700

    uas pweb

---
+++ Jual-Telur/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar navbar-dark" style="background-color: #02d934";>
+<div class='container'>
+  <a class="navbar-brand" href="index.php">Jual Telur Ayam</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link" href="index.php"></a>
+      <a class="nav-item nav-link" href="view_cart.php">Keranjang tiket (<?php echo $cart->get_cart_count();?>)</a>
+    </div>
+  </div>
+  </div>
+</nav>
